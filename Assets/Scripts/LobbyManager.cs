f265e73d24fb05adf7a2a002a1db9d9d922ef7c5
nahue <nahuelbianca90@gmail.com>
2026-02-01 10:57:51 -0300

    muertes

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -161,7 +161,7 @@ public class LobbyManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("MergeScene");
+        SceneManager.LoadScene("MergeScene 1");
     }
 
     private void OnDestroy(){
