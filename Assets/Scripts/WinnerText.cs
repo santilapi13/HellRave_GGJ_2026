9e2d8c5b33e198cc591f0d08df60feb51336792a
santilapi13 <santilapiana02@gmail.com>
2026-02-01 14:38:20 -0300

    enviar de game over al lobby

--- Assets/Scripts/WinnerText.cs
+++ Assets/Scripts/WinnerText.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinnerText : MonoBehaviour
 {
     [SerializeField] private TMPro.TextMeshProUGUI winnerText;
+    [SerializeField] private TMPro.TextMeshProUGUI winnerTextShadow;
 
     void Start()
     {
         var winner = GameManager.Instance.GetWinnersName();
         winnerText.text = winner;
+        winnerTextShadow.text = winner;
+        Invoke("ChangeToLobby", 5);
+    }
+
+    private void ChangeToLobby()
+    {
+        SceneManager.LoadScene("Lobby");
     }
 }
